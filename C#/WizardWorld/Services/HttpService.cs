ffe6c17d7d6d20b320189bd70084e9145ce42463
Eric Slutz <eric.slutz@icloud.com>
2022-08-26 23:41:03 -0400

    created httpservice

---
+++ C#/WizardWorld/Services/HttpService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace WizardWorld.Services
+{
+	public class HttpService
+	{
+		private readonly HttpClient _httpClient = new();
+
+
+		public async Task<string> GetAsync(Uri endpointUrl)
+		{
+            var resp = await _httpClient.GetAsync(endpointUrl);
+
+            return await resp.Content.ReadAsStringAsync();
+        }
+	}
+}
+
